273c080f0f7a8524ccac8fbe873ae922317e0f39
RKrosnicki <112870610+RKrosnicki@users.noreply.github.com>
2023-08-04 19:50:41 +0200

    Add files via upload

--- Day-24-Letter-Names-Replace/main.py
+++ Day-24-Letter-Names-Replace/main.py
@@ -1,1 +1,12 @@
+PLACEHOLDER = "[name]"
 
+with open("./Input/Names/invited_names.txt") as invited_names:
+    names_list = invited_names.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as starting_letter:
+    letter = starting_letter.read()
+
+for name in names_list:
+    stripped_name = name.strip()
+    with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as f:
+        f.writelines(letter.replace(PLACEHOLDER, stripped_name))

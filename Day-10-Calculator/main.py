1244166ca43589e33f08b59d39f6cedce0d91609
RKrosnicki <112870610+RKrosnicki@users.noreply.github.com>
2023-08-04 19:21:27 +0200

    Create main.py

---
+++ Day-10-Calculator/main.py
@@ -0,0 +1,51 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    num1 = float(input("What's the first number? "))
+
+    for symbol in operations:
+        print(symbol)
+    function = input("Choose wisely: ")
+
+    num2 = float(input("What's the second number? "))
+
+    answer = operations[function](num1, num2)
+
+    print(f"{num1} {function} {num2} = {answer}")
+
+    done = input("Are you done yet? (y/n)")
+
+    while done == "n":
+        print(f"Last answer is {answer}")
+        for symbol in operations:
+            print(symbol)
+        function = input("Choose wisely: ")
+        num3 = float(input("What's next number? "))
+        num1 = answer
+        answer = operations[function](num1, num3)
+        print(f"{num1} {function} {num3} = {answer}")
+        done = input("Are you done yet? (y/n) ")
+    if done == "y":
+        clear() # comment it if you're on PyCharm
+        print(logo)
+        calculator()
+
+print(logo)
+calculator()

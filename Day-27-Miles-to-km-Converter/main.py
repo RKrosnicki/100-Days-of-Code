859ea152019b0edae406399288a16e032edd18a7
Radek <r.krosnicki@gmail.com>
2023-08-11 14:51:57 +0200

    kopytko

--- Day-27-Miles-to-km-Converter/main.py
+++ Day-27-Miles-to-km-Converter/main.py
@@ -1,8 +1,15 @@
 import tkinter
 
+def mile_to_km():
+    result = float(miles.get()) * 1.609
+    result_label.config(text=result)
+
 window = tkinter.Tk()
 window.title("Miles to km converter")
-window.minsize(width=300, height=200)
+window.config(padx=20, pady=20)
+
+miles = tkinter.Entry(width=7)
+miles.grid(column=1, row=0)
 
 miles_label = tkinter.Label(text="miles")
 miles_label.grid(column=2, row=0)
@@ -12,18 +19,11 @@ is_equal_label.grid(column=0, row=1)
 
 result_label = tkinter.Label(text="0")
 result_label.grid(column=1, row=1)
-
-def calculate():
-    result = float(miles.get()) * 1.609
-    result_label.config(text=result)
     
 km_label = tkinter.Label(text="km")
 km_label.grid(column=2, row=1)
 
-my_button = tkinter.Button(text="Calculate", command=calculate)
+my_button = tkinter.Button(text="Calculate", command=mile_to_km)
 my_button.grid(column=1, row=2)
-
-miles = tkinter.Entry(width=10)
-miles.grid(column=1, row=0)
 
 window.mainloop()

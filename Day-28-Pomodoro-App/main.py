e5f458323710cc0830a4ab3be49ce1fe4d3e517e
Radek <r.krosnicki@gmail.com>
2023-08-13 23:38:10 +0200

    kopytko

--- Day-28-Pomodoro-App/main.py
+++ Day-28-Pomodoro-App/main.py
@@ -10,8 +10,21 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 break_time = False
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    global break_time
+    global timer
+    global count
+    canvas.itemconfig(timer_text, text="00:00")
+    count = 0
+    window.after_cancel(timer)
+    reps = 0
+    break_time = False
+    start_button.config(text="start")
+    title_label.config(text="Timer")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -19,24 +32,30 @@ def start_timer():
     global break_time
 
     if not break_time:
+        title_label.config(text="WORKING", fg=GREEN)
         break_time = 1
         count_down(WORK_MIN * 60)
-        #count_down(20)
     else:
         reps += 1
-        if break_time and reps > 0 and reps % 5 == 0:
+        if break_time and reps > 0 and reps % 4 == 0:
+            title_label.config(text="RESTING LONGER", fg=RED)
             reps = 0
             break_time = False
             count_down(LONG_BREAK_MIN * 60)
-            #count_down(10)
         else:
             break_time = False
+            title_label.config(text="RESTING", fg=PINK)
             count_down(SHORT_BREAK_MIN * 60)
-            #count_down(5)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     global reps
+    global timer
+    if break_time:
+        start_button.config(text="start break")
+    else:
+        start_button.config(text="start work")
+
     mins = count // 60
     seconds = count % 60
     if seconds < 10:
@@ -44,13 +63,13 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{mins}:{seconds}")
 
     if count > 0:
-        window.after(10, count_down, count-1)
-    kopytko = reps * "✔"
+        timer = window.after(10, count_down, count-1)
+    elif count == 0:
+        window.attributes("-topmost", True)
+        window.attributes("-topmost", False)
     checkmarks.config(text=reps * "✔")
 
-
 # ---------------------------- UI SETUP ------------------------------- #
-
 
 window = Tk()
 window.title("Pomodoro")
@@ -68,7 +87,7 @@ title_label.grid(column=1, row=0)
 start_button = Button(text="start", width=10, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="reset", width=10)
+reset_button = Button(text="reset", width=10, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 checkmarks = Label(text=" ", fg=GREEN, bg=YELLOW, font=(50))

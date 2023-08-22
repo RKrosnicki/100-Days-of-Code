fe13f0d6417efb82e05ff9f29d39d269aa0fa6c5
Radek <r.krosnicki@gmail.com>
2023-08-22 16:58:23 +0200

    kopytko

--- Day-30/main.py
+++ Day-30/main.py
@@ -1,15 +1,25 @@
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed.")
+
+height = float(input("Height in meters: "))
+weight = float(input("Weight in kilograms: "))
+
+if height > 2.5:
+    raise ValueError("Human Height should not be over 2.5m")
+
+bmi = weight / height ** 2
+
+print(bmi)
